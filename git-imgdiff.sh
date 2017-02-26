190b8b2f17eab220592fbd0af00c6609ff4b3af7
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2017-02-26 15:44:19 +0100

    Fix PNG differ for paths with spaces

--- git-imgdiff.sh
+++ git-imgdiff.sh
@@ -6,16 +6,16 @@ rm -rf $DIFF_PATH $MONTAGE_PATH
 
 # Try to diff the images and create a montage with the original on
 # the left, diff in the middle and the new one on the right.
-DIFF=$(compare $2 $1 png:- > $DIFF_PATH)
+DIFF=$(compare "$2" "$1" png:- > $DIFF_PATH)
 
 # XXX: for some reason IG exits with 1 when it successfully creates the diff
 if [[ $? == 1 ]]; then
-  montage -label Original $2 -label Diff $DIFF_PATH -label New $1 \
+  montage -label Original "$2" -label Diff $DIFF_PATH -label New "$1" \
     -tile x1 -geometry '433x+4+4>' $MONTAGE_PATH
 else
   # If the images have different sizes the diff will fail.
   # In this case, create a montage with just the original and the new one.
-  montage -label Original $2 -label New $1 \
+  montage -label Original "$2" -label New "$1" \
     -tile x1 -geometry '650x+4+4>' $MONTAGE_PATH
 fi
 
