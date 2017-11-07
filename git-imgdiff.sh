2c592d0b16a3f9c633a4e7d93de66024656581c8
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2017-11-07 20:50:45 +0200

    [git-imgdiff] Noop in case of removed images

--- git-imgdiff.sh
+++ git-imgdiff.sh
@@ -4,6 +4,12 @@ DIFF_PATH=/tmp/diff.png
 MONTAGE_PATH=/tmp/montage.png
 rm -rf $DIFF_PATH $MONTAGE_PATH
 
+# This script will be called for removed images as well so we just noop for that
+# case.
+if [ ! -f "$1" ]; then
+  exit 0
+fi
+
 # Try to diff the images and create a montage with the original on
 # the left, diff in the middle and the new one on the right.
 DIFF=$(compare "$2" "$1" png:- > $DIFF_PATH)
